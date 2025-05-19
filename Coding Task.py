665ff8c8e15e3be9d41fc47226ebb0557c01a4bc
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2025-05-19 11:22:03 +0100

    Exercicio 1

---
+++ Coding Task.py
@@ -0,0 +1,7 @@
+#	11. Write a Python program that prints the following text exactly as shown (including blank lines and formatting):
+
+#Hello!
+#Welcome to Python programming.
+#Let's begin!
+
+print("Hello!\nWelcome to Python Programing.\nLet's Begin!")
