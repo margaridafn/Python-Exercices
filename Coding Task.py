c7f217baf67aaae07a96d3e76d2aeea661455ce9
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2025-05-19 12:36:44 +0100

    Pergunta 2

--- Coding Task.py
+++ Coding Task.py
@@ -5,3 +5,7 @@
 #Let's begin!
 
 print("Hello!\nWelcome to Python Programing.\nLet's Begin!")
+
+#Write a Python program that:
+#Assigns an integer, a float, a string, a Boolean, and a None value to five different variables.
+#Prints all the variables on the same line.
