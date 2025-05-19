53310b0d9fb01c4d46942f63adb6011c4cfe0093
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2025-05-19 12:44:15 +0100

    Exercicio 2

--- Coding Task.py
+++ Coding Task.py
@@ -1,4 +1,4 @@
-#	11. Write a Python program that prints the following text exactly as shown (including blank lines and formatting):
+#Write a Python program that prints the following text exactly as shown (including blank lines and formatting):
 
 #Hello!
 #Welcome to Python programming.
@@ -7,5 +7,14 @@
 print("Hello!\nWelcome to Python Programing.\nLet's Begin!")
 
 #Write a Python program that:
+
 #Assigns an integer, a float, a string, a Boolean, and a None value to five different variables.
 #Prints all the variables on the same line.
+
+i=3
+f=5.07
+s="This is a string"
+b=False
+n=None
+
+print(i, f, s, b, n)
