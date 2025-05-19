8dc6ffe570e702c745112f96bc13f8de9753bf79
Margarida Nogueira <margarida.nogueira.05@gmail.com>
2025-05-19 17:37:38 +0100

    Exercicios 3 e 4

--- Coding Task.py
+++ Coding Task.py
@@ -18,3 +18,26 @@ b=False
 n=None
 
 print(i, f, s, b, n)
+
+#Escreve um código que calcula e imprime o seguinte:
+
+#O quociente inteiro e o resto da divisão de 23 por 5
+# A potência de 2 elevado a 4
+#O resultado da seguinte expressão: (3 + 2) * 4 ** 2
+
+d=23//5
+r=23%5
+p=2**4
+e=(3+2)*4**2
+
+print(d, r, p, e)
+
+#Write a small Python program that:
+
+#Creates a variable called counter and assigns it the value 5
+#Increases the value of counter by 3 using a shortcut operator
+#Prints the final value of counter
+
+counter=5
+counter+=3
+print(counter)
